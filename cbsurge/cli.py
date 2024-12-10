11c026e4ba4374113580c1e5d56b35237cf8a562
Joseph Thuha <josephthuha@gmail.com>
2024-12-10 16:52:17 +0300

    Feat/download worldpop (#6)
    * feat: download worlpop unadjusted age structure data
    
    * comment out some code used to test
    
    * add tqdm in upload
    
    * fix: add .env.example
    
    * update requirements.txt
    
    * comment out testing code
    
    * fix: awaited coroutine cannot be awaited
    
    * awaitin twice fix
    
    * feat: add cog validation and logs
    
    * add statistics
    
    * feat: implement new approach download
    
    * feat: implement cli for downloading worldpop
    
    * fix: check file exist as the first step in processing
    
    * fix: azure progress bar and change param for local download
    
    * update cli
    
    * fix: add new deps to requirements.txt
    
    * refactor: create azure module
    
    * feat: rename azure_upload to azure
    
    * refactor
    
    * upload multiple files
    
    * feat: azure module
    
    * change name of called function and add constants
    
    * make click work with async
    
    * remove unused code

--- cbsurge/cli.py
+++ cbsurge/cli.py
@@ -1,11 +1,16 @@
+import click as click
+
 from cbsurge.admin import admin
-import click
+from cbsurge.exposure.population import population
+
+
 @click.group
 
 def cli():
     """Main CLI for the application."""
     pass
 cli.add_command(admin)
+cli.add_command(population)
 
 
 if __name__ == '__main__':
---
+++ cbsurge/constants.py
@@ -0,0 +1,3 @@
+AGE_STRUCTURES_ROOT_URL = "https://hub.worldpop.org/geodata"
+AZURE_BLOB_CONTAINER_NAME = "stacdata"
+AZURE_FILESHARE_NAME = "cbrapida"

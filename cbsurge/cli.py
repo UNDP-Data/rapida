d88f699a3f6a2e5ad83155fcd07717629eb6a365
Jin Igarashi <igarashi.jin@water-gis.com>
2024-12-13 17:47:40 +0900

    feat: added stats compute command (#85)
    * feat: added stats compute command
    
    * add pytest
    
    * fix: allow multiple rasters to compute
    
    * add test in readme
    
    * fix: changed input and output to arguments

--- cbsurge/cli.py
+++ cbsurge/cli.py
@@ -2,6 +2,7 @@ import click as click
 
 from cbsurge.admin import admin
 from cbsurge.exposure.population import population
+from cbsurge.stats import stats
 
 
 @click.group
@@ -11,6 +12,7 @@ def cli():
     pass
 cli.add_command(admin)
 cli.add_command(population)
+cli.add_command(stats)
 
 
 if __name__ == '__main__':

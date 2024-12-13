c7951b85e989bd974557d3e664e382eedc110eb2
iferencik <92793531+iferencik@users.noreply.github.com>
2024-12-13 11:15:27 +0100

    buildings download  (#86)
    * pmtiles attempt
    
    * async PMTiles buildings
    
    * preliminary buildings from pmtiles
    
    * finish pmtiles download
    
    * finish building download
    
    * move iterlen to util
    
    * docstrings
    
    * fgb buildiongs source
    
    * clean imports
    
    * fast FGB
    
    * finish buildings download
    
    ---------
    
    Co-authored-by: Ioan Ferencik <ioan.ferencik@undop.org>

--- cbsurge/cli.py
+++ cbsurge/cli.py
@@ -1,6 +1,17 @@
 import click as click
 
 from cbsurge.admin import admin
+
+from cbsurge.exposure.builtenv import builtenv
+import click
+
+@click.group
+def cli(ctx):
+    """Main CLI for the application."""
+    pass
+cli.add_command(admin)
+cli.add_command(builtenv)
+
 from cbsurge.exposure.population import population
 from cbsurge.stats import stats
 
@@ -13,6 +24,7 @@ def cli():
 cli.add_command(admin)
 cli.add_command(population)
 cli.add_command(stats)
+
 
 
 if __name__ == '__main__':
--- cbsurge/util.py
+++ cbsurge/util.py
@@ -1,1 +1,78 @@
-r
+import httpx
+import logging
+from osgeo import gdal
+import itertools
+import click
+import os
+logger = logging.getLogger(__name__)
+
+def fetch_drivers():
+    d = dict()
+    for i in range(gdal.GetDriverCount()):
+
+        drv = gdal.GetDriver(i)
+        d[drv.ShortName] = drv.GetMetadataItem(gdal.DMD_EXTENSIONS)
+    return d
+
+def http_get_json(url=None, timeout=None):
+    """
+    Generic HTTP get function using httpx
+    :param url: str, the url to be fetched
+    :param timeout: httpx.Timeout instance
+    :return: python dict representing the result as parsed json
+    """
+    assert timeout is not None, f'Invalid timeout={timeout}'
+    with httpx.Client(timeout=timeout) as client:
+        response = client.get(url)
+        response.raise_for_status()
+        if response.status_code == 200:
+            return response.json()
+
+
+def validate(url=None, timeout=10):
+    """
+    Generic HTTP get function using httpx
+    :param url: str, the url to be fetched
+    :param timeout: httpx.Timeout instance
+    :return: python dict representing the result as parsed json
+    """
+    with httpx.Client(timeout=timeout) as client:
+        response = client.head(url, timeout=timeout)
+        response.raise_for_status()
+
+
+
+def generator_length(gen):
+    """
+    compute the no of elems inside a generator
+    :param gen:
+    :return:
+    """
+    gen1, gen2 = itertools.tee(gen)
+    length = sum(1 for _ in gen1)  # Consume the duplicate
+    return length, gen2  # Return the length and the unconsumed generator
+
+
+class BboxParamType(click.ParamType):
+    name = "bbox"
+    def convert(self, value, param, ctx):
+        try:
+            bbox = [float(x.strip()) for x in value.split(",")]
+            fail = False
+        except ValueError:  # ValueError raised when passing non-numbers to float()
+            fail = True
+
+        if fail or len(bbox) != 4:
+            self.fail(
+                f"bbox must be 4 floating point numbers separated by commas. Got '{value}'"
+            )
+
+        return bbox
+
+def validate_path(src_path=None):
+    assert os.path.isabs(src_path), f'{src_path} has to be a file'
+    out_folder, file_name = os.path.split(src_path)
+    assert os.path.exists(out_folder), f'Folder {src_path} has to exist'
+
+    if os.path.exists(src_path):
+        assert os.access(src_path, os.W_OK), f'Can not write to {src_path}'

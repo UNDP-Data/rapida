5420e5c64a8a8eb6993c66b92be8f526d9471d91
Jin Igarashi <igarashi.jin@water-gis.com>
2024-12-13 17:47:40 +0900

    feat: added stats compute command (#85)
    * feat: added stats compute command
    
    * add pytest
    
    * fix: allow multiple rasters to compute
    
    * add test in readme
    
    * fix: changed input and output to arguments

---
+++ cbsurge/stats/__init__.py
@@ -0,0 +1,84 @@
+import logging
+import click
+from cbsurge.stats.ZonalStats import ZonalStats
+
+
+@click.group()
+def stats():
+    f"""Command line interface for {__package__} package"""
+    pass
+@stats.command(no_args_is_help=True)
+@click.argument('input_file', type=click.Path())
+@click.argument('output_file', type=click.Path())
+@click.option('-r', '--raster',
+              required=True,
+              multiple=True,
+              type = str,
+              help='The list of input raster files'
+              )
+@click.option('-o', '--operation',
+              required=True,
+              multiple=True,
+              type=click.Choice([
+                  'cell_id', 'center_x', 'center_y', 'coefficient_of_variation', 'count',
+                  'coverage', 'frac', 'majority', 'max', 'max_center_x', 'max_center_y',
+                  'mean', 'median', 'min', 'min_center_x', 'min_center_y',
+                  'minority', 'quantile', 'stdev', 'sum', 'unique',
+                  'values', 'variance', 'variety', 'weighted_frac','weighted_mean',
+                  'weighted_stdev', 'weighted_sum', 'weighted_variance', 'weights'
+              ], case_sensitive=True),
+              help=
+              """
+              Operations to perform (e.g., "sum", "mean", "count"). Can be specified multiple times. 
+              See all available operations at https://isciences.github.io/exactextract/operations.html
+              """
+              )
+@click.option('-c', '--column',
+              required=False,
+              multiple=True,
+              help=
+              """
+              list of column names for each operation. The number of columns must match the number of operations multiplied by the number of rasters.
+              If this option is not specified, raster file name is used as prefix of column names.
+              For example, if aaa.tif is specified, column name will be 'aaa_sum'.
+              """
+              )
+@click.option('-s','--srid',
+              required=False,
+              default=3857,
+              type=int,
+              help='SRID for output vector file. Default is 3857',
+              )
+@click.option('--debug',
+              is_flag=True,
+              default=False,
+              help="Set log level to debug"
+              )
+def compute(
+        input_file=None,
+        output_file=None,
+        raster=None,
+        srid=3857,
+        operation=None,
+        column=None,
+        debug=False):
+    """
+    This command compute zonal statistics with given raster files from a vector file, and save the result to output file.
+
+    Arguments: \n
+        The below arguments must be specified to execute the command apart from optional parameters.
+
+        - input_file: input file must be vector file such as geojson, flatgeobuf.\n
+        - output_file: output file format supports Shapefile (.shp), GeoJSON (.geojson), FlatGeobuf (.fgb) and GeoPackage (.gpkg)
+
+    Usage: \n
+        The below command provides how to use the command to compute zonal statistics.\n
+        python -m cbsurge.cli stats compute --help
+
+    Example: \n
+        python -m cbsurge.cli stats compute ./cbsurge/stats/tests/assets/admin2.geojson ./cbsurge/stats/tests/assets/admin2_stats.fgb -r ./cbsurge/stats/tests/assets/rwa_m_5_2020_constrained_UNadj.tif -r ./cbsurge/stats/tests/assets/rwa_f_5_2020_constrained_UNadj.tif -o sum -c male_5_sum -c female_5_sum
+    """
+    logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
+    with ZonalStats(input_file, target_srid=54009) as st:
+        st.compute(raster, operations=operation, operation_cols=column)
+        st.write(output_file, target_srid=srid)
---
+++ cbsurge/stats/tests/test_ZonalStats.py
@@ -0,0 +1,118 @@
+import os
+import pytest
+from cbsurge.stats.ZonalStats import ZonalStats
+
+TEST_DIR = "./cbsurge/stats/tests"
+
+def get_test_path(relative_path):
+    """Helper function to generate absolute paths for test assets."""
+    return os.path.abspath(f"{TEST_DIR}/{relative_path}")
+
+@pytest.mark.parametrize(
+    "input_file, input_raster, operations, operation_cols, expected_columns, output_file, num_columns",
+    [
+        # Test case 1: Single operation
+        (
+            "assets/admin2.geojson",
+            ["assets/rwa_m_5_2020_constrained_UNadj.tif"],
+            ["sum"],
+            None,
+            ["rwa_m_5_2020_constrained_UNadj_sum"],
+            "assets/admin2_stats.fgb",
+            10,
+        ),
+        # Test case 2: Multiple operations
+        (
+            "assets/admin2.geojson",
+            ["assets/rwa_m_5_2020_constrained_UNadj.tif"],
+            ["sum", "count"],
+            None,
+            ["rwa_m_5_2020_constrained_UNadj_sum", "rwa_m_5_2020_constrained_UNadj_count"],
+            None,
+            11,
+        ),
+        # Test case 3: Custom column names
+        (
+            "assets/admin2.geojson",
+            ["assets/rwa_m_5_2020_constrained_UNadj.tif"],
+            ["sum", "median"],
+            ["male_5_age_sum", "memale_5_age_median"],
+            ["male_5_age_sum", "memale_5_age_median"],
+            None,
+            11,
+        ),
+        # Test case 4: Output GPKG
+        (
+            "assets/admin2.geojson",
+            ["assets/rwa_m_5_2020_constrained_UNadj.tif"],
+            ["sum", "median"],
+            None,
+            ["rwa_m_5_2020_constrained_UNadj_sum", "rwa_m_5_2020_constrained_UNadj_median"],
+            "assets/admin2_stats.gpkg",
+            11,
+        ),
+        # Test case 5: multi rasters with single operation
+        (
+            "assets/admin2.geojson",
+            ["assets/rwa_m_5_2020_constrained_UNadj.tif", "assets/rwa_f_5_2020_constrained_UNadj.tif"],
+            ["sum"],
+            None,
+            ["rwa_m_5_2020_constrained_UNadj_sum", "rwa_f_5_2020_constrained_UNadj_sum"],
+            "assets/admin2_stats_multi.fgb",
+            11,
+        ),
+        # Test case 6: multi rasters with multi operations
+        (
+            "assets/admin2.geojson",
+            ["assets/rwa_m_5_2020_constrained_UNadj.tif", "assets/rwa_f_5_2020_constrained_UNadj.tif"],
+            ["sum", "median"],
+            None,
+            ["rwa_m_5_2020_constrained_UNadj_sum", "rwa_m_5_2020_constrained_UNadj_median", "rwa_f_5_2020_constrained_UNadj_sum", "rwa_f_5_2020_constrained_UNadj_median"],
+            "assets/admin2_stats_multi.fgb",
+            13,
+        ),
+        # Test case 7: multi rasters with single operation and custom column names
+        (
+            "assets/admin2.geojson",
+            ["assets/rwa_m_5_2020_constrained_UNadj.tif", "assets/rwa_f_5_2020_constrained_UNadj.tif"],
+            ["sum"],
+            ["male 5 sum", "female 5 sum"],
+            ["male 5 sum", "female 5 sum"],
+            "assets/admin2_stats_multi.fgb",
+            11,
+        ),
+        # Test case 8: multi rasters with multi operations
+        (
+            "assets/admin2.geojson",
+            ["assets/rwa_m_5_2020_constrained_UNadj.tif", "assets/rwa_f_5_2020_constrained_UNadj.tif"],
+            ["sum", "median"],
+            ["male 5 sum", "male 5 median", "female 5 sum", "female 5 median"],
+            ["male 5 sum", "male 5 median", "female 5 sum", "female 5 median"],
+            "assets/admin2_stats_multi.fgb",
+            13,
+        ),
+    ]
+)
+def test_zonal_stats(input_file, input_raster, operations, operation_cols, expected_columns, output_file, num_columns):
+    """Test raster files with various operations and configurations."""
+    input_file = get_test_path(input_file)
+    for index, raster in enumerate(input_raster):
+        input_raster[index] = get_test_path(input_raster[index])
+    output_file = get_test_path(output_file) if output_file else None
+
+    with ZonalStats(input_file, target_srid=54009) as st:
+        gdf = st.compute(
+            input_raster,
+            operations=operations,
+            operation_cols=operation_cols)
+
+        for col in expected_columns:
+            assert col in gdf.columns, f"Column '{col}' not found in result."
+
+        assert len(gdf.columns) == num_columns, f"The number of columns {len(gdf.columns)} does not match the expected number of columns."
+
+        if output_file:
+            st.write(output_file, target_srid=3857)
+            assert os.path.exists(output_file), f"Output file {output_file} not found."
+            if os.path.exists(output_file):
+                os.remove(output_file)

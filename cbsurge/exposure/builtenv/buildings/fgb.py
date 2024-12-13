670b3d7602e4ea0ba9a865acb2b70fcfd7b75523
Ioan Ferencik <ioan.ferencik@undop.org>
2024-12-13 11:13:02 +0100

    finish buildings download

--- cbsurge/exposure/builtenv/buildings/fgb.py
+++ cbsurge/exposure/builtenv/buildings/fgb.py
@@ -1,56 +1,130 @@
+import json
+import os.path
+
 from cbsurge.exposure.builtenv.buildings.fgbgdal import get_countries_for_bbox_osm, GMOSM_BUILDINGS_ROOT
 from pyogrio.raw import open_arrow, write_arrow
 import logging
 import time
-import tempfile
-import os
 from tqdm import tqdm
-from cbsurge.util import generator_length
+from pyogrio.core import read_info
+from osgeo import ogr, osr, gdal
+
+
+
+
 logger = logging.getLogger(__name__)
 
+ARROWTYPE2OGRTYPE = {'string':ogr.OFTString, 'double':ogr.OFTReal, 'int64':ogr.OFTInteger64, 'int':ogr.OFTInteger}
 
-from osgeo import ogr, osr
-srs_prj = osr.SpatialReference()
-srs_prj.ImportFromEPSG(4326)
-def download1(bbox=None, out_path=None, in_batches=True):
-    nbuildings = 0
+
+def add_fields_to_layer(layer=None, template_layer_info=None):
+
+    types = dict([(e[3:], getattr(ogr, e)) for e in dir(ogr) if e.startswith('OFT')])
+    for field_dict in template_layer_info['layers'][0]['fields']:
+        layer.CreateField(ogr.FieldDefn(field_dict['name'], types[field_dict['type']]))
+
+
+def download_pyogrio(bbox=None, out_path=None, batch_size:[int,None]=1000):
+    """
+    Download/stream buildings from VIDA buildings using pyogrio/pyarrow API
+
+    :param bbox: iterable of floats, xmin, ymin, xmax,ymax
+    :param out_path: str, full path where the buildings layer will be written
+    :param batch_size: int, default=1000, the max number of buildings to download in one batch
+    If supplied, the buildings are downloaded in batches otherwise they are streamd through pyarrow library
+    :return:
+    """
+
+
+    countries = get_countries_for_bbox_osm(bbox=bbox)
+    assert len(countries)> 0, f'The bounding box {bbox} does not intersect any country. Please make sure it makes sense!'
+
     with ogr.GetDriverByName('FlatGeobuf').CreateDataSource(out_path) as dst_ds:
-        dst_lyr = dst_ds.CreateLayer('buildings', geom_type=ogr.wkbPolygon, srs=srs_prj)
-        for country in get_countries_for_bbox_osm(bbox=bbox):
+        for country in countries :
             remote_country_fgb_url = f'/vsicurl/{GMOSM_BUILDINGS_ROOT}/country_iso={country}/{country}.fgb'
-            # local_country_fgb_url = os.path.join(tmp_dir, f'buildings_{country}.fgb')
-            if in_batches:
-                with open_arrow(remote_country_fgb_url, bbox=bbox, use_pyarrow=True, batch_size=1000) as source:
+            if batch_size is not None:
+                with open_arrow(remote_country_fgb_url, bbox=bbox, use_pyarrow=True, batch_size=batch_size) as source:
                     meta, reader = source
                     fields = meta.pop('fields')
-                    crs = meta['crs']
-                    for field_name, field_type in  zip(reader.schema.names,reader.schema.types):
-                        if field_type == 'string':
-                            ogrft = ogr.OFTString
-                        if field_type == 'double':
-                            ogrft = ogr.OFTReal
-                        if field_type == 'int64':
-                            ogrft = ogr.OFTInteger64
-                        if 'geometry' in field_name or 'wkb' in field_name:continue
-                        dst_lyr.CreateField(ogr.FieldDefn(field_name, ogrft))
+                    schema = reader.schema
 
-                    # non_empty_batches = (b for b in reader if b.num_rows > 0)
-                    # no_batches, batches = generator_length(non_empty_batches)
-                    bar_format = "{desc}: {n_fmt} buildings... {postfix}"
-                    with tqdm(total=None, desc=f'Downloaded', bar_format=bar_format) as pbar:
-                        for batch in reader:
-                            logger.debug(f'Writing {batch.num_rows} records')
-                            pbar.set_postfix_str(f'from {country}...', refresh=True)
-                            #write_arrow(batch, out_path,layer='buildings',driver='FlatGeobuf',append=True, metadata=meta)
-                            dst_lyr.WritePyArrow(batch)
-                            nbuildings+=batch.num_rows
-                            pbar.update(batch.num_rows)
-
-                        pbar.set_postfix_str('Finished')
+                    if dst_ds.GetLayerCount() == 0:
+                        src_epsg = int(meta['crs'].split(':')[-1])
+                        src_srs = osr.SpatialReference()
+                        src_srs.ImportFromEPSG(src_epsg)
+                        dst_lyr = dst_ds.CreateLayer('buildings', geom_type=ogr.wkbPolygon, srs=src_srs)
+                        for name in schema.names:
+                            if 'wkb' in name or 'geometry' in name:continue
+                            field = schema.field(name)
+                            field_type = ARROWTYPE2OGRTYPE[field.type]
+                            dst_lyr.CreateField(ogr.FieldDefn(name, field_type))
+                    logger.info(f'Downloading buildings in batches from {remote_country_fgb_url}')
+                    for batch in reader:
+                        logger.debug(f'Writing {batch.num_rows} records')
+                        dst_lyr.WritePyArrow(batch)
             else:
                 with open_arrow(remote_country_fgb_url, bbox=bbox, use_pyarrow=False) as source:
                     meta, reader = source
-                    write_arrow(reader, out_path,layer='buildings',driver='FlatGeobuf',append=True, metadata=meta)
+                    src_epsg = int(meta['crs'].split(':')[-1])
+                    src_srs = osr.SpatialReference()
+                    src_srs.ImportFromEPSG(src_epsg)
+                    logger.info(f'Streaming buildings from {remote_country_fgb_url}')
+                    write_arrow(reader, out_path,layer='buildings',driver='FlatGeobuf',append=True,
+                                geometry_name='wkb_geometry', geometry_type='Polygon', crs=src_srs.ExportToWkt())
+    info = read_info(out_path, layer='buildings')
+    logger.info(f'{info["features"]} buildings were downloaded from {",".join(countries)} country datasets')
+
+
+
+def download_gdal(bbox=None, out_path=None, batch_size:[int, None]=1000):
+    """
+    Download/stream buildings from VIDA buildings using gdal/pyarrow API
+    :param bbox: iterable of floats, xmin, ymin, xmax,ymax
+    :param out_path: str, full path where the buildings layer will be written
+    :param batch_size: int, default=1000, the max number of buildings to be downloaded in one batch
+    If supplied, the buildings are downloaded in batches otherwise they are streamd through pyarrow library.
+    Batch downloading should be preferred in case of large bounding boxes/area
+
+    :return:
+    """
+
+
+    countries = get_countries_for_bbox_osm(bbox=bbox)
+    assert len(countries)> 0, f'The bounding box {bbox} does not intersect any country. Please make sure it makes sense!'
+    buildings = 0
+    with ogr.GetDriverByName('FlatGeobuf').CreateDataSource(out_path) as dst_ds:
+
+        for country in get_countries_for_bbox_osm(bbox=bbox) :
+            remote_country_fgb_url = f'/vsicurl/{GMOSM_BUILDINGS_ROOT}/country_iso={country}/{country}.fgb'
+            with ogr.Open(remote_country_fgb_url, gdal.OF_READONLY) as src_ds:
+                src_lyr = src_ds.GetLayer(0)
+                src_lyr.SetSpatialFilterRect(*bbox)
+                if batch_size is not None:
+                    stream = src_lyr.GetArrowStream([f"MAX_FEATURES_IN_BATCH={batch_size}"])
+                else:
+                    stream = src_lyr.GetArrowStream()
+                schema = stream.GetSchema()
+                if dst_ds.GetLayerCount() == 0:
+                    src_srs = src_lyr.GetSpatialRef()
+                    dst_lyr = dst_ds.CreateLayer('buildings', geom_type=ogr.wkbPolygon, srs=src_srs)
+                    for i in range(schema.GetChildrenCount()):
+                        if 'wkb' in schema.GetChild(i).GetName() or 'geometry' in schema.GetChild(i).GetName():continue
+                        dst_lyr.CreateFieldFromArrowSchema(schema.GetChild(i))
+                if batch_size is not None:
+                    logger.info(f'Downloading buildings in batches from {remote_country_fgb_url}')
+                else:
+                    logger.info(f'Streaming buildings from {remote_country_fgb_url}')
+                while True:
+                    array = stream.GetNextRecordBatch()
+                    if array is None:
+                        break
+                    assert dst_lyr.WriteArrowBatch(schema, array) == ogr.OGRERR_NONE
+                    buildings+= array.GetLength()
+    logger.info(f'{buildings} buildings were downloaded from {",".join(countries)} country datasets')
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -78,7 +152,7 @@ if __name__ == '__main__':
     start = time.time()
     #asyncio.run(download(bbox=bbox))
 
-    download1(bbox=bbox, out_path=out_path)
+    download_gdal(bbox=bbox, out_path=out_path, batch_size=3000)
 
     end = time.time()
     print((end-start))

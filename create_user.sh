bd5ebffceb9a3ef70047b62af8bdb8f27b537723
Jin Igarashi <igarashi.jin@water-gis.com>
2025-01-16 18:23:34 +0900

    fix: separate home and data folder in the container.

--- create_user.sh
+++ create_user.sh
@@ -2,7 +2,6 @@
 
 USERNAME=$1
 PASSWORD=$2
-GROUPNAME=cbsurge
 
 # skip if user already exists
 if id "$USERNAME" &>/dev/null; then
@@ -14,7 +13,7 @@ else
     echo "User $USERNAME created."
 
     # Add the user to the group
-    usermod -aG $GROUPNAME "$USERNAME"
+    usermod -aG $GROUP_NAME "$USERNAME"
     echo "User $USERNAME added to $GROUPNAME group."
 
     # Grant sudo access (optional)
@@ -22,10 +21,8 @@ else
     echo "User $USERNAME granted sudo privileges."
 
     # change user home directory
-    USER_HOME_DIR=$HOME/$USERNAME
-    mkdir -p "$USER_HOME_DIR"
-    chown "$USERNAME:$GROUPNAME" "$USER_HOME_DIR"  # Ensure ownership is set
-    usermod -d "$USER_HOME_DIR" "$USERNAME"
-    rm -rf /home/$USERNAME
-    echo "User $USERNAME home directory changed to $USER_HOME_DIR."
+    USER_DATA_DIR=$DATA_DIR/$USERNAME
+    mkdir -p "$USER_DATA_DIR"
+    chown "$USERNAME:$GROUP_NAME" "$USER_DATA_DIR"
+    echo "User $USERNAME data directory was created at $USER_DATA_DIR."
 fi

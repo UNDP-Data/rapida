b45bc4a1719320275654acb76f047cdaa88488cb
Joseph Thuha <josephthuha@gmail.com>
2025-01-20 11:47:14 +0300

    feat: sync and aggregation of worldpop data (#89)
    * fix: cleanup and aggregation
    
    * WIP: process specific age and sex groups:
    
    * run aggregations
    
    * feat: process all combinations implementation
    
    * fix: run it for all countries with sync command
    
    * remove persistent temp files
    
    * check for if aggregates exist
    
    * fix: update metadata for sizes
    
    * WIP: debugging
    
    * fix: breaking before all blocks are processed
    
    * upload data
    
    * refactor: to upload for all
    
    * refactor: process remaining
    
    * refactor: process remaining
    
    * WIP
    
    * fix: update to use  and  instead of  and
    
    * add download-path argument
    
    * refactor: improve population workflow
    
    * shuffle list
    
    * shuffle age_sex constant
    
    * chunk1
    
    * chunk2
    
    * chunk3
    
    * feat: cleanup

--- cbsurge/exposure/population/constants.py
+++ cbsurge/exposure/population/constants.py
@@ -3,22 +3,27 @@
 AZ_ROOT_FILE_PATH = "worldpop"
 CONTAINER_NAME = "stacdata"
 WORLDPOP_AGE_MAPPING = {
-    "0-12": 0,
-    "1-4": 1,
-    "5-9": 5,
-    "10-14": 10,
-    "15-19": 15,
-    "20-24": 20,
-    "25-29": 25,
-    "30-34": 30,
-    "35-39": 35,
-    "40-44": 40,
-    "45-49": 45,
-    "50-54": 50,
-    "55-59": 55,
-    "60-64": 60,
-    "65-69": 65,
-    "70-74": 70,
-    "75-79": 75,
-    "80+": 80
+    "child": [0, 14],
+    "active": [15, 64],
+    "elderly": [65, 100],
 }
+SEX_MAPPING = {
+    "M": "male",
+    "F": "female",
+}
+
+DATA_YEAR = 2020
+
+AGESEX_STRUCTURE_COMBINATIONS = [
+            {"sexes": ["male"], "age_group": None, "label": "male_total"},
+            {"sexes": ["female"], "age_group": None, "label": "female_total"},
+            {"sexes": ["male"], "age_group": "active", "label": "male_active"},
+            {"sexes": ["female"], "age_group": "active", "label": "female_active"},
+            {"sexes": ["male"], "age_group": "child", "label": "male_child"},
+            {"sexes": ["female"], "age_group": "child", "label": "female_child"},
+            {"sexes": ["male"], "age_group": "elderly", "label": "male_elderly"},
+            {"sexes": ["female"], "age_group": "elderly", "label": "female_elderly"},
+            {"sexes": ["male", "female"], "age_group": "elderly", "label": "elderly_total"},
+            {"sexes": ["male", "female"], "age_group": "child", "label": "child_total"},
+            {"sexes": ["male", "female"], "age_group": "active", "label": "active_total"},
+        ]

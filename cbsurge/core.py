82c060ca2407abb7f12e58cae99890abe7a7afe2
Ioan Ferencik <ioan.ferencik@undop.org>
2025-01-22 22:12:08 +0100

    pop variables

--- cbsurge/core.py
+++ cbsurge/core.py
@@ -1,6 +1,6 @@
 import json
-import re
-
+from sympy.parsing.sympy_parser import parse_expr
+from osgeo_utils import gdal_calc
 from pydantic import BaseModel, Field
 import typing
 import pkgutil
@@ -16,19 +16,38 @@ def list_surge_packages(path=os.path.dirname(__file__), valid_names = ('exposure
 class Variable(BaseModel):
     id: str
     name: str
-    file_patterns: typing.List[str]
-    dependencies: typing.Optional[typing.Any] = None
-    extractor:str = r"\{([^}]+)\}"
+    source: str = None
+    file: str = None
+    _extractor_:str = r"\{([^}]+)\}"
+    _default_operators_ = '+-/*'
+    computed: typing.Optional[bool] = None
+    operators: typing.Optional[set[str]] = None
+    _source_file_:str = None
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.resolve(**kwargs)
+
+    def resolve_file(self, **kwargs):
+        if 'iso3_country_code' in kwargs:
+            kwargs['iso3_country_code_lower'] = kwargs['iso3_country_code_lower'].lower()
+        variables = set(re.findall(self._extractor_, self.file))
+        for varname in variables:
+            assert varname in kwargs, f'"{varname}" kwarg is required to generate source files'
+        return self.file.format(**kwargs)
+
+    def compute(self):
+        parsed_expr = parse_expr(self.source)
+        variables = parsed_expr.free_symbols
 
 
-    def source_files(self, **kwargs):
-        files = []
-        for patt in self.file_patterns:
-            variables = set(re.findall(self.extractor, patt))
-            for varname in variables:
-                assert varname in kwargs, f'"{varname}" kwarg is required to generate source files'
-            files.append(patt.format(**kwargs))
-        return files
+
+    def resolve(self, **kwargs):
+        if self.source is not None:
+            operators = set(self._default_operators_).intersection(self.source)
+            if operators:
+                self.operators = operators
+                self.computed = True
+                self.compute()
 
 
 
@@ -37,8 +56,9 @@ if __name__ == '__main__':
 
     #list_surge_packages()
     with open('./exposure/population/variables.json') as vsrc:
-        json_data = vsrc.read()
-        v = Variable.model_validate_json(json_data, strict=False)
-        print(v)
-        s = v.source_files(year=2020, iso3_country_code = 'ALB')
-        print(s)
+        json_data = json.load(vsrc)
+        for v in json_data:
+            if v['id'] == 'dependency':
+                vr = Variable(**v)
+
+                print(vr)

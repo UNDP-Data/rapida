dae95125cfbf45c5b7d181e3b3b78c01c02ad0a9
Ioan Ferencik <ioan.ferencik@undop.org>
2025-02-01 02:03:40 +0100

    finish polygon import

--- cbsurge/project.py
+++ cbsurge/project.py
@@ -4,11 +4,84 @@ import click
 import logging
 import shutil
 from cbsurge.session import Session
+from osgeo import gdal, ogr, osr
+from collections import UserDict
+
 
 logger = logging.getLogger(__name__)
+gdal.UseExceptions()
 
+class Config(UserDict):
+    def __init__(self, file_path, **kwargs):
+        """
+        Initialize the AutoSaveDict with a target JSON filename.
+        If the file exists, load its contents; otherwise, start with an empty dict
+        (or with any provided initial values).
 
+        :param file_path: Path to the JSON file where the dictionary will be saved.
+        :param args: Positional arguments passed to dict().
+        :param kwargs: Keyword arguments passed to dict().
+        """
+        self.file_path = file_path
 
+        # If the file exists, load its contents
+        if os.path.exists(self.file_path):
+            try:
+                with open(self.file_path, 'r') as f:
+                    loaded_data = json.load(f)
+            except (json.JSONDecodeError, IOError) as e:
+                print(f"Warning: Could not load JSON from {self.file_path}: {e}")
+                loaded_data = {}
+        else:
+            loaded_data = {}
+
+        # Merge with any additional data provided during instantiation.
+        # Values provided in args/kwargs will override those loaded from the file.
+        loaded_data.update(dict(**kwargs))
+
+        # Initialize the underlying dictionary with the merged data.
+        super().__init__(**loaded_data)
+        self._save()  # Write the initial state (in case the file didn't exist)
+
+    def __setitem__(self, key, value):
+        """Set the item and save the dictionary to the JSON file."""
+        super().__setitem__(key, value)
+        self._save()
+
+    def __delitem__(self, key):
+        """Delete the item and save the dictionary to the JSON file."""
+        super().__delitem__(key)
+        self._save()
+
+    def update(self, *args, **kwargs):
+        """Update the dictionary and save the changes."""
+        super().update( *args, **kwargs)
+        self._save()
+
+    def clear(self):
+        """Clear the dictionary and save the changes."""
+        super().clear()
+        self._save()
+
+    def pop(self, key, *args):
+        """Remove the specified key and save the dictionary."""
+        value = super().pop(key, *args)
+        self._save()
+        return value
+
+    def popitem(self):
+        """Remove and return an arbitrary (key, value) pair and save the dictionary."""
+        item = super().popitem()
+        self._save()
+        return item
+
+    def _save(self):
+        """Write the current state of the dictionary to the JSON file."""
+        try:
+            with open(self.file_path, 'w') as f:
+                json.dump(self.data, f, indent=4)
+        except Exception as e:
+            print(f"Error saving AutoSaveDict to {self.file_path}: {e}")
 
 
 class Project:
@@ -16,40 +89,79 @@ class Project:
     data_folder = None
     geopackage_file:str = None
     name:str = None
-    def __init__(self, folder:str=None, ):
+    def __init__(self, folder:str=None, polygons:str=None, mask:str=None, projection='ESRI:54009' ):
+
         folder = os.path.abspath(folder)
-        *rest, name = folder.split(os.path.sep)
-        self.name = name
-        self.folder = folder
-        self.data_folder = os.path.join(self.folder, 'data')
-        self.geopackage_file = os.path.join(self.folder, self.data_folder, f'{self.name}.gpkg')
-        self.config_file = os.path.join(self.folder, self.config_file_name)
-        if not os.path.exists(self.folder):
-            os.makedirs(self.folder)
-            logger.debug(f'Creating project folder ...')
-            self.config = dict(
-                name=self.name,
-                config_file=self.config_file,
-                folder=self.folder,
-                data_folder=self.data_folder
-            )
-            logger.debug(f'Creating project config file ...')
-            self.serialize()
-            logger.info(f'Project {self.name} is located in {self.folder}')
+        config_file = os.path.join(folder, self.config_file_name)
+        if os.path.exists(config_file):
+            self.config = Config(file_path=config_file)
+            for k, v in self.config.items():
+                self.__setattr__(k, v)
+
         else:
-            if os.path.exists(self.config_file):
-                self.config = self.deserialize()
+            self.folder = folder
+            *rest, name = self.folder.split(os.path.sep)
+            self.name = name
+            self.config_file = os.path.join(self.folder, self.config_file_name)
+            self.data_folder = os.path.join(self.folder, 'data')
+            self.geopackage_file = os.path.join(self.data_folder, f'{self.name}.gpkg')
+            if not os.path.exists(self.folder):
+                logger.debug(f'Creating project folder ...')
+                os.makedirs(self.folder)
+                self.config = Config(file_path=self.config_file,
+                                     name=self.name,
+                                     config_file=self.config_file,
+                                     folder=self.folder,
+
+                                     )
+                logger.debug(f'Creating project config file ...')
+                logger.info(f'Project {self.name} was created in {self.folder}')
             else:
-                logger.info(f'Creating project config file ...')
-                self.config = dict(
-                    name=self.name,
-                    config_file=self.config_file,
-                    folder=self.folder,
-                    data_folder=self.data_folder
-                )
-                self.serialize()
-                logger.info(f'Project {self.name} is located in {self.folder}')
+                if os.path.exists(self.config_file):
+                    self.config = Config(file_path=self.config_file)
+
+                else:
+                    logger.info(f'Creating project config file ...')
+                    self.config = Config(file_path=self.config_file,
+                                         name=self.name,
+                                         config_file=self.config_file,
+                                         folder=self.folder,
+                                         )
+
+                    logger.info(f'Project {self.name} is located in {self.folder}')
+
+        if polygons is not None:
+            with gdal.OpenEx(polygons) as poly_ds:
+                lcount = poly_ds.GetLayerCount()
+                if lcount > 1:
+                    lnames = list()
+                    for i in range(lcount):
+                        l = poly_ds.GetLayer(i)
+                        lnames.append(l.GetName())
+                    #click.echo(f'{polygons} contains {lcount} layers: {",".join(lnames)}')
+                    layer_name = click.prompt(
+                        f'{polygons} contains {lcount} layers: {",".join(lnames)} Please type/select  one or pres enter to skip if you wish to use default value',
+                        type=str, default=lnames[0])
+                else:
+                    layer_name = poly_ds.GetLayer(0).GetName()
+                if not os.path.exists(self.data_folder):
+                    os.makedirs(self.data_folder)
+                gdal.VectorTranslate(self.geopackage_file, poly_ds, format='GPKG',reproject=True, dstSRS=projection,
+                                 layers=[layer_name], layerName='polygons', geometryType='PROMOTE_TO_MULTI', makeValid=True)
+        if mask is not  None:
+            try:
+                vm_ds = gdal.OpenEx(mask, gdal.OF_VECTOR)
+            except RuntimeError as ioe:
+                if 'supported' in str(ioe):
+                    vm_ds = None
+                else:
+                    raise
+            if vm_ds is not None:
+                pass
+
         assert self.is_valid, f'{self} is not valid'
+
+
 
     def __str__(self):
         txt =   f'''
@@ -57,19 +169,9 @@ class Project:
     folder:          {self.folder}
     config file:     {self.config_file}
     data folder:     {self.data_folder}
-    geopackage file: {self.geopackage_file}                                            
+    geopackage file: {self.geopackage_file}
                 '''
         return txt
-
-    def deserialize(self):
-        if os.path.exists(self.config_file):
-            with open(self.config_file, "r", encoding="utf-8") as data_h:
-                return json.load(data_h)
-
-    def serialize (self):
-
-        with open(self.config_file, "w", encoding="utf-8") as cfg_fh:
-            cfg_fh.write(json.dumps(self.config, ensure_ascii=False, indent=4))
 
     @property
     def is_valid(self):
@@ -91,8 +193,14 @@ def project():
 
 
 @click.command(no_args_is_help=True)
-@click.argument('folder', required=True, type=click.Path())
-def init(folder):
+
+@click.option('-f', '--folder', required=True, type=str,
+              help='Full path to the project folder or a name representing a folder in the current directory' )
+@click.option('-p', '--polygons', required=True, type=str,
+              help='Full path to the vector polygons dataset in any OGR supported format' )
+@click.option('-m', '--mask', required=False, type=str,
+              help='Full path to the mask dataset in any GDAL/OGR supported format. Can be vector or raster' )
+def create(folder=None, polygons=None, mask=None):
     """
     Create a Rapida project
 
@@ -104,14 +212,19 @@ def init(folder):
         if os.path.exists(afolder):
             prj = Project(folder=afolder)
             if prj.is_valid:
-                logger.info(f'Project {folder} exists at {afolder}')
+                logger.info(f'Project {folder} exists at {afolder}. You can continue.')
+                prj = Project(folder=folder, polygons=polygons, mask=mask)
+                session.config['project'] = prj.folder
+                session.save_config()
                 return
         if not os.path.isabs(folder):
             if click.confirm(f'Project {folder} will be created in "{os.getcwd()}". Do you want to continue?', abort=True):
                 pass
-        prj = Project(folder=folder)
+
+        prj = Project(folder=folder, polygons=polygons, mask=mask)
         session.config['project'] = prj.folder
         session.save_config()
+
 
 
 @click.command()
@@ -120,7 +233,7 @@ def show():
     with Session() as session:
         last_project = session.config.get('project', None)
         if last_project is None:
-            logger.info(f'No Rapida propject has been defined yet')
+            logger.info(f'No Rapida project has been defined yet')
         else:
             prj = Project(folder=last_project)
             logger.info(f'Current Rapida project: {prj}')
@@ -150,6 +263,6 @@ def delete(project):
 
 
 project.add_command(show)
-project.add_command(init)
+project.add_command(create)
 project.add_command(delete)
 

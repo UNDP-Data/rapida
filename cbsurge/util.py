5d449f075610f9b573322b1cdca10dd9a8fff735
iferencik <92793531+iferencik@users.noreply.github.com>
2025-02-12 16:20:08 +0100

    rename year|base_year|target_year to correctly parse year in source t… (#179)
    * rename year|base_year|target_year to correctly parse year in source templates
    
    * add util func to detect code is running in jupyter
    
    ---------
    
    Co-authored-by: Ioan Ferencik <ioan.ferencik@undop.org>

--- cbsurge/util.py
+++ cbsurge/util.py
@@ -204,6 +204,17 @@ def validate_path(src_path=None):
     if os.path.exists(src_path):
         assert os.access(src_path, os.W_OK), f'Can not write to {src_path}'
 
+def in_notebook():
+    """check if code is being executed in a jupyter notebook"""
+    try:
+        from IPython import get_ipython
+        if 'IPKernelApp' not in get_ipython().config:  # pragma: no cover
+            return False
+    except ImportError:
+        return False
+    except AttributeError:
+        return False
+    return True
 
 class FQFunctionNameFormatter(logging.Formatter):
     """Custom formatter to display the fully qualified function name in logs."""

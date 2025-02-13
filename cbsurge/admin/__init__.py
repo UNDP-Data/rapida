90df9d3f6730c85866a474f3a4cfc11cd1a6d52e
Joseph Thuha <josephthuha@gmail.com>
2025-02-13 15:08:27 +0300

    feat: remove layer_name argument and implement code to manually remove outout file where it exists (#193)
    * feat: remove layer_name argument and implement code to manually remove file in cases where it cannot be overwritten
    
    * fix: use @click.argument instead of @click.option for destination path
    
    * fix: update variables in arguments
    
    * move argument before option in admin

--- cbsurge/admin/__init__.py
+++ cbsurge/admin/__init__.py
@@ -1,5 +1,7 @@
 
 import logging
+import os
+
 from cbsurge.admin.osm import fetch_admin as fetch_osm_admin, ADMIN_LEVELS
 from cbsurge.admin.ocha import fetch_admin as fetch_ocha_admin
 from cbsurge.util import BboxParamType
@@ -18,6 +20,10 @@ def save(geojson_dict=None, dst_path=None, layer_name=None):
     :param layer_name: str, the layer name
     :return:
     """
+    # if there is a file remove it first to avoid raising a gdal error in cases where the file cannot be overwritten
+    if dst_path and os.path.exists(dst_path):
+        os.remove(dst_path)
+
     if dst_path is not None:
         with gdal.OpenEx(json.dumps(geojson_dict, indent=2)) as src:
             src_layer_name = src.GetLayer(0).GetName()
@@ -36,6 +42,9 @@ def admin():
 
 
 @admin.command(no_args_is_help=True)
+
+@click.argument('destination_path', type=click.Path())
+
 @click.option('-b', '--bbox', required=True, type=BboxParamType(),
               help='Bounding box xmin/west, ymin/south, xmax/east, ymax/north' )
 @click.option('-l','--admin_level',
@@ -62,20 +71,7 @@ def admin():
     help="Precision level for H3 indexing (default is 7)."
 )
 
-@click.option(
-    '--dst_path',
-    type=click.Path(),
-    default=None,
-    required=True,
-    help="The absolute path to an OGR dataset."
-)
-@click.option(
-    '--layer-name',
-    type=str,
-    default=None,
-    required=True,
-    help="The name of the layer."
-)
+
 
 @click.option('--debug',
 
@@ -85,7 +81,7 @@ def admin():
 )
 
 
-def osm(bbox=None,admin_level=None, osm_level=None, clip=False, h3id_precision=7,dst_path=None, layer_name=None, debug=False,):
+def osm(bbox=None,admin_level=None, osm_level=None, clip=False, h3id_precision=7, destination_path=None, debug=False,):
     """
     Fetch admin boundaries from OSM
 
@@ -106,7 +102,7 @@ def osm(bbox=None,admin_level=None, osm_level=None, clip=False, h3id_precision=7
 
     To save the result as a file, for instance, the following command can be executed to extract admin 0 data for Rwanda and Burundi as GeoJSON file:
 
-    rapida admin osm -b "27.767944,-5.063586,31.734009,-0.417477" -l 0 > osm.geojson
+    rapida admin osm -b "27.767944,-5.063586,31.734009,-0.417477" -l 0 osm.geojson
     """
     logger = logging.getLogger('rapida')
     if debug:
@@ -116,11 +112,14 @@ def osm(bbox=None,admin_level=None, osm_level=None, clip=False, h3id_precision=7
     if not geojson:
         logger.error('Could not extract admin boundaries from OSM for the provided bbox')
         return
-    save(geojson_dict=geojson, dst_path=dst_path, layer_name=layer_name)
+    save(geojson_dict=geojson, dst_path=destination_path, layer_name=f'admin{admin_level}')
 
 
 
 @admin.command(no_args_is_help=True)
+
+@click.argument('destination_path', type=click.Path())
+
 @click.option('-b', '--bbox', required=True, type=BboxParamType(),
               help='Bounding box xmin/west, ymin/south, xmax/east, ymax/north' )
 @click.option('-l','--admin_level',
@@ -143,20 +142,8 @@ def osm(bbox=None,admin_level=None, osm_level=None, clip=False, h3id_precision=7
     help="Precision level for H3 indexing (default is 7)."
 )
 
-@click.option(
-    '--dst_path',
-    type=click.Path(),
-    default=None,
-    required=True,
-    help="The absolute path to an OGR dataset."
-)
-@click.option(
-    '--layer-name',
-    type=str,
-    default=None,
-    required=True,
-    help="The name of the layer."
-)
+
+
 
 @click.option('--debug',
 
@@ -166,7 +153,7 @@ def osm(bbox=None,admin_level=None, osm_level=None, clip=False, h3id_precision=7
 )
 
 
-def ocha(bbox=None,admin_level=None,  clip=False, h3id_precision=7, dst_path=None, layer_name=None, debug=False ):
+def ocha(bbox=None,admin_level=None,  clip=False, h3id_precision=7, destination_path=None, debug=False ):
     """
     Fetch admin boundaries from OCHA COD
 
@@ -184,7 +171,7 @@ def ocha(bbox=None,admin_level=None,  clip=False, h3id_precision=7, dst_path=Non
     To save the result as a file, for instance, the following command can be executed to extract admin 0 data for Rwanda and Burundi as GeoJSON file:
 
 
-    rapida admin ocha -b 33.681335,-0.131836,35.966492,1.158979 -l 2 --clip --dst_path /data/admocha.fgb --layer-name abc
+    rapida admin ocha -b 33.681335,-0.131836,35.966492,1.158979 -l 2 --clip /data/admocha.fgb --layer-name abc
     """
 
     logger = logging.getLogger('rapida')
@@ -194,5 +181,5 @@ def ocha(bbox=None,admin_level=None,  clip=False, h3id_precision=7, dst_path=Non
     if not geojson:
         logger.error('Could not extract admin boundaries from OCHA for the provided bbox')
         return
-    save(geojson_dict=geojson, dst_path=dst_path, layer_name=layer_name)
+    save(geojson_dict=geojson, dst_path=destination_path, layer_name=f'admin{admin_level}')
 

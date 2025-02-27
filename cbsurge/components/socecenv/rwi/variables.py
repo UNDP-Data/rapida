b1960723cf99059952cbb5d6356579bea2a5e6c9
Jin Igarashi <igarashi.jin@water-gis.com>
2025-02-27 21:43:09 +0900

    refactor: variable.py

--- cbsurge/components/socecenv/rwi/variables.py
+++ cbsurge/components/socecenv/rwi/variables.py
@@ -6,25 +6,15 @@ logger = logging.getLogger(__name__)
 def generate_variables():
     """
     Generate relative wealth index variables dict
-    :return:
+    :return RWI variables definition
     """
 
     variables = OrderedDict()
 
-    #dependencies
-    variables['rwi_mean'] = dict(
-        title='Mean of relative wealth index',
-        source='geohub:/api/datasets/fcde6ab53a79657a27906b3248a1979d',
-        operator="mean"
-    )
-    variables['rwi_min'] = dict(
-        title='Minimum of relative wealth index',
-        source='geohub:/api/datasets/fcde6ab53a79657a27906b3248a1979d',
-        operator="min"
-    )
-    variables['rwi_max'] = dict(
-        title='Maximum of relative wealth index',
-        source='geohub:/api/datasets/fcde6ab53a79657a27906b3248a1979d',
-        operator="max"
-    )
+    for operator in ['mean', 'min', 'max']:
+        variables[f'rwi_{operator}'] = dict(
+            title=f'{operator} of relative wealth index',
+            source='geohub:/api/datasets/fcde6ab53a79657a27906b3248a1979d',
+            operator=operator
+        )
     return variables

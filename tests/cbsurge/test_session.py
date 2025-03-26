b378c1bb34b10abf4933450061b5c8bb73e3c54e
Jin Igarashi <igarashi.jin@water-gis.com>
2025-03-26 14:11:48 +0900

    feat: implemented rapida delete command (#283)

--- tests/cbsurge/test_session.py
+++ tests/cbsurge/test_session.py
@@ -9,11 +9,10 @@ def test_session():
         s.set_root_data_folder("~/cbsurge")
 
         assert s.get_blob_service_account_url() == "https://test_account.blob.core.windows.net"
-        assert s.get_file_share_account_url() == "https://test_account.file.core.windows.net/test_share"
+        assert s.get_file_share_account_url() == "https://test_account.file.core.windows.net"
 
         assert s.get_blob_service_account_url(account_name="aaa") == "https://aaa.blob.core.windows.net"
-        assert s.get_file_share_account_url(account_name="aaa") == "https://aaa.file.core.windows.net/test_share"
-        assert s.get_file_share_account_url(account_name="aaa", share_name="bbb") == "https://aaa.file.core.windows.net/bbb"
+        assert s.get_file_share_account_url(account_name="aaa") == "https://aaa.file.core.windows.net"
 
         assert s.get_root_data_folder(False) == "~/cbsurge"
         assert s.get_root_data_folder(True) == os.path.expanduser("~/cbsurge")

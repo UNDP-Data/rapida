d564b8ade18e84800bd89e1852fb935a407d7ad1
Jin Igarashi <igarashi.jin@water-gis.com>
2025-04-11 17:39:25 +0900

    fix: simplify `rapida init` command interface and message (#332)
    * fix: removed prompts to set internally used azure information, and exposed them as environmental varialbes
    
    * fix: deleted unused root data folder from session and init command prompts

--- cbsurge/initialize.py
+++ cbsurge/initialize.py
@@ -1,7 +1,6 @@
 import logging
 import click
 import os
-import shutil
 from cbsurge.session import Session
 from cbsurge.components.population.variables import generate_variables as gen_pop_vars
 from cbsurge.components.buildings.variables import generate_variables as gen_bldgs_vars
@@ -13,8 +12,15 @@ from cbsurge.components.landuse.variables import generate_variables as gen_landu
 from cbsurge.components.gdp.variables import generate_variables as gen_gdp_vars
 from cbsurge.util.setup_logger import setup_logger
 
+
 logger = logging.getLogger(__name__)
 
+
+AZURE_STORAGE_ACCOUNT=os.environ.get("AZURE_STORAGE_ACCOUNT", "undpgeohub")
+AZURE_PUBLISH_CONTAINER_NAME=os.environ.get("AZURE_PUBLISH_CONTAINER_NAME", "rapida")
+AZURE_STAC_CONTAINER_NAME=os.environ.get("AZURE_STAC_CONTAINER_NAME", "stacdata")
+AZURE_FILE_SHARE_NAME=os.environ.get("AZURE_FILE_SHARE_NAME", "cbrapida")
+GEOHUB_ENDPOINT=os.environ.get("GEOHUB_ENDPOINT", "https://geohub.data.undp.org")
 
 def setup_prompt(session: Session):
     auth = session.authenticate()
@@ -27,58 +33,22 @@ def setup_prompt(session: Session):
     else:
         click.echo("Authentication successful.")
 
-    click.echo("We need more information to setup from you.")
-
-    # project root data folder
-    root_data_folder = None
-    absolute_root_data_folder = None
-    while not(root_data_folder is not None and os.path.exists(absolute_root_data_folder)):
-        data_folder = click.prompt("Please enter project root folder to store all data. Enter to skip if use default value", default="~/cbsurge")
-        absolute_root_data_folder = os.path.expanduser(data_folder)
-
-        if os.path.exists(absolute_root_data_folder):
-            if click.confirm("The folder already exists. Yes to overwrite, No/Enter to use existing folder", default=False):
-                shutil.rmtree(absolute_root_data_folder)
-                click.echo(f"Removed folder {absolute_root_data_folder}")
-                os.makedirs(absolute_root_data_folder)
-                click.echo(f"The project root folder was created at {absolute_root_data_folder}")
-                root_data_folder = data_folder
-            else:
-                click.echo(f"Use {absolute_root_data_folder} as the root folder.")
-                root_data_folder = data_folder
-        else:
-            os.makedirs(absolute_root_data_folder)
-            click.echo(f"The project root folder was created at {absolute_root_data_folder}")
-            root_data_folder = data_folder
-    session.set_root_data_folder(root_data_folder)
 
     # azure blob container setting
-    account_name = click.prompt('Please enter account name for UNDP Azure. Enter to skip if use default value',
-                                type=str, default='undpgeohub')
-    session.set_account_name(account_name)
-    click.echo(f"account name: {account_name}")
-
-    publish_container_name = click.prompt('Please enter UNDP Azure container name of publishing project outcome. Enter to skip if use default value',
-                                  type=str, default='rapida')
-    session.set_publish_container_name(publish_container_name)
-    click.echo(f"publish container name: {publish_container_name}")
-
-    stac_container_name = click.prompt('Please enter container name for UNDP Azure STAC. Enter to skip if use default value',
-                                  type=str, default='stacdata')
-    session.set_stac_container_name(stac_container_name)
-    click.echo(f"stac container name: {stac_container_name}")
+    session.set_account_name(AZURE_STORAGE_ACCOUNT)
+    logger.debug(f"account name: {session.get_account_name()}")
+    session.set_publish_container_name(AZURE_PUBLISH_CONTAINER_NAME)
+    logger.debug(f"publish container name: {session.get_publish_container_name()}")
+    session.set_stac_container_name(AZURE_STAC_CONTAINER_NAME)
+    logger.debug(f"stac container name: {session.get_stac_container_name()}")
 
     # azure file share setting
-    share_name = click.prompt('Please enter share name for UNDP Azure. Enter to skip if use default value',
-                              type=str, default='cbrapida')
-    session.set_file_share_name(share_name)
-    click.echo(f"file share name: {share_name}")
+    session.set_file_share_name(AZURE_FILE_SHARE_NAME)
+    logger.debug(f"file share name: {session.get_file_share_name()}")
 
     # geohub endpoint setting
-    geohub_endpoint = click.prompt('Please enter URL of GeoHub endpoint. Enter to skip if use default value',
-                              type=str, default='https://geohub.data.undp.org')
-    session.set_geohub_endpoint(geohub_endpoint)
-    click.echo(f"GeoHub endpoint: {geohub_endpoint}")
+    session.set_geohub_endpoint(GEOHUB_ENDPOINT)
+    logger.debug(f"GeoHub endpoint: {session.get_geohub_endpoint()}")
 
     session.save_config()
     vars_dict = {
@@ -95,7 +65,8 @@ def setup_prompt(session: Session):
     }
     session.config.update(vars_dict)
     session.save_config()
-    click.echo('Setting up was successfully done!')
+    click.echo(f"Initialization was done. config file was saved to {session.get_config_file_path()}")
+
 
 
 @click.command(short_help='initialize RAPIDA tool')

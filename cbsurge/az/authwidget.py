bfdf13fd25010ec44ed497d6e91d84540ed13ce2
Ioan Ferencik <ioan.ferencik@undop.org>
2025-04-11 09:54:05 +0200

    async auth + ntb widgets

--- cbsurge/az/authwidget.py
+++ cbsurge/az/authwidget.py
@@ -1,14 +1,21 @@
+import ipywidgets
 import jwt
 import ipywidgets as widgets
 from IPython.display import display
 from cbsurge.util.in_notebook import in_notebook
 from cbsurge.az.surgeauth import SurgeTokenCredential
-out = widgets.Output(layout={'padding': '25px'})
+import logging
+import asyncio
+logger = logging.getLogger(__name__)
+
+#out = widgets.Output(layout={'padding': '25px'})
+
 user_name_w = widgets.Text(
     value='',
     placeholder='...enter your UNDP email address',
     description='Email:',
-    disabled=False
+    disabled=False,
+    layout=widgets.Layout(visibility='visible')
 )
 
 password_w = widgets.Password(
@@ -35,24 +42,39 @@ mfa_number_w = widgets.IntText(
     disabled=True
 )
 
-auth_widget = widgets.HBox(children=[user_name_w, password_w, auth_button], layout=widgets.Layout(border='1px solid gray', padding='15px', align_items='flex-end'))
-auth_container = widgets.VBox(children=[out, auth_widget])
+feedback_html = widgets.HTML(value="", layout={'border': '0px solid grey', 'background':'silver', 'padding':'5px'})
+
+auth_widget = widgets.HBox(children=[user_name_w, password_w, auth_button], layout=widgets.Layout(padding='2px', justify_content='flex-end', align_items='center'))
+auth_container = widgets.VBox(
+    children=[auth_widget,feedback_html],
+    layout=widgets.Layout(
+        align_items='flex-end',  # Aligns the children (feedback_html and auth_widget) to the right
+        justify_content='flex-start',  # Aligns the children vertically at the top
+        width='100%'  # Ensures the container stretches across the available space
+    )
+)
 
 
-@out.capture(clear_output=True)
-def authenticate(b):
 
+#@out.capture(clear_output=True)
+async def authenticate():
     email, passwd = user_name_w.value, password_w.value
     if email and passwd:
-        print(email, passwd)
         assert '@' in email, f'Invalid email address "{email}"'
         assert passwd not in ('', None), f'Invalid password'
         credential = SurgeTokenCredential()
-        return credential.get_token(credential.STORAGE_SCOPE, auth_widget=None)
+        res = await credential.get_token_async(credential.STORAGE_SCOPE, email=email, password=passwd, mfa_widget=feedback_html)
+        feedback_html.value=''
+        info = decode_id_token(credential.token['id_token'])
+        auth_button.description = f'{info["name"]}'
+        # auth_button.layout.visibility = 'hidden'
+        user_name_w.layout = widgets.Layout(display='none')
+        password_w.layout = widgets.Layout(display='none')
 
+def on_click(b):
+    asyncio.ensure_future(authenticate())
 
-
-@out.capture(clear_output=True)
+#@out.capture(clear_output=True)
 def decode_id_token(id_token):
     """Decodes an Azure AD ID token and extracts user details."""
     try:
@@ -60,18 +82,24 @@ def decode_id_token(id_token):
     except Exception as e:
         print("Error decoding token:", e)
 
-auth_button.on_click(authenticate)
+auth_button.on_click(on_click)
 
 def load_ui():
-    if in_notebook():
-        credential = SurgeTokenCredential()
-        token, exp_time = credential.fetch_token(credential.STORAGE_SCOPE)
-        if credential.token is not None:
-            info = decode_id_token(credential.token['id_token'])
-            auth_button.description = f'{info["name"]}'
-            user_name_w.layout.visibility = 'hidden'
-            password_w.layout.visibility = 'hidden'
+    credential = SurgeTokenCredential()
+    display(auth_container)
+    if credential.authenticated:
+        info = decode_id_token(credential.token['id_token'])
+        auth_button.description = f'{info["name"]}'
+        #auth_button.layout.visibility = 'hidden'
+        user_name_w.layout = widgets.Layout(display='none')
+        password_w.layout = widgets.Layout(display='none')
+        #feedback_html.value = f'<b>UNDP user:  {info["name"]}</b> '
 
-        display(auth_container)
-        return auth_widget
+    else:
+        if auth_button.disabled:
+            auth_button.disabled = False
+        if auth_button.description != 'Authenticate':
+            auth_button.description = 'Authenticate'
+
+
 

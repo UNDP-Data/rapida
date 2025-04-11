f9b98186e3dd9363eee59856ed8b34508bb64243
Thuhaa <josephthuha@gmail.com>
2025-04-11 18:52:01 +0300

    refactor: remove geopandas reference

--- cbsurge/util/gpd_overlay.py
+++ cbsurge/util/gpd_overlay.py
@@ -9,15 +9,15 @@ def chunk_gdf(gdf, chunk_size):
     for i in range(0, len(gdf), chunk_size):
         yield gdf.iloc[i:i + chunk_size]
 
-def process_chunk(chunk_rows, roads_layer, roads_sindex, crs):
+def process_chunk(chunk_rows, overlay_df, overlay_df_sindex, crs):
     results = []
 
     for idx, row in chunk_rows.iterrows():
         single_row_gdf = gpd.GeoDataFrame([row], geometry='geometry', crs=crs)
         poly_geom = single_row_gdf.geometry.iloc[0]
 
-        possible_matches_index = list(roads_sindex.intersection(poly_geom.bounds))
-        possible_matches = roads_layer.iloc[possible_matches_index]
+        possible_matches_index = list(overlay_df_sindex.intersection(poly_geom.bounds))
+        possible_matches = overlay_df.iloc[possible_matches_index]
         precise_matches = possible_matches[possible_matches.intersects(poly_geom)].copy()
         precise_matches.loc[:, "geometry"] = precise_matches.geometry.intersection(poly_geom)
 
@@ -31,14 +31,13 @@ def process_chunk(chunk_rows, roads_layer, roads_sindex, crs):
     if results:
         return pd.concat(results, ignore_index=True)
 
-    return pd.DataFrame(columns=roads_layer.columns)
+    return pd.DataFrame(columns=overlay_df.columns)
 
 def run_overlay(polygons_df=None, overlay_df=None, chunk_size=100, **kwargs):
     progress = kwargs.get('progress')
-    # chunk_size = 2
     max_workers = 4
     final_results = gpd.GeoDataFrame()
-    roads_sindex = overlay_df.sindex
+    overlay_df_sindex = overlay_df.sindex
     chunks = list(chunk_gdf(polygons_df, chunk_size))
     if progress:
         overlay_task = progress.add_task(description=f'[red]Running overlay for {len(chunks)} chunks')
@@ -47,7 +46,7 @@ def run_overlay(polygons_df=None, overlay_df=None, chunk_size=100, **kwargs):
         overlay_task = progress.add_task(description=f'[green]Running overlay for {len(chunks)} chunks')
     with ProcessPoolExecutor(max_workers=max_workers) as executor:
         futures = [
-            executor.submit(process_chunk, chunk, overlay_df, roads_sindex, polygons_df.crs)
+            executor.submit(process_chunk, chunk, overlay_df, overlay_df_sindex, polygons_df.crs)
             for chunk in chunks
         ]
         for i, future in enumerate(futures):
@@ -62,7 +61,7 @@ def run_overlay(polygons_df=None, overlay_df=None, chunk_size=100, **kwargs):
     if len(final_results) > 0:
         return final_results
     else:
-        logging.warning("No intersecting roads found for any polygons.")
+        logging.warning("No intersecting rows from overlay_df found for any polygons.")
     logging.info("run_overlay complete")
 
 

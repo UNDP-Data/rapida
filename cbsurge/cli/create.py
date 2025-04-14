b9b78fd75dc00282925d87dc38f5c823398b64cc
Jin Igarashi <igarashi.jin@water-gis.com>
2025-04-14 20:40:02 +0900

    fix: set project name as a first positional argument, and allow users to create a project in any folder by using optional second positional argument.

--- cbsurge/cli/create.py
+++ cbsurge/cli/create.py
@@ -10,8 +10,8 @@ logger = logging.getLogger(__name__)
 
 
 @click.command(no_args_is_help=True, short_help='create a RAPIDA project in a new folder')
-@click.option('-n', '--name', required=True, type=str,
-              help='Name representing a new folder in the current directory' )
+@click.argument('project_name', type=str, required=True)
+@click.argument('destination_folder', type=click.Path(), required=False)
 @click.option('-p', '--polygons', required=True, type=str,
               help='Full path to the vector polygons dataset in any OGR supported format' )
 @click.option('-m', '--mask', required=False, type=str,
@@ -23,19 +23,41 @@ logger = logging.getLogger(__name__)
               default=False,
               help="Set log level to debug"
               )
-def create(name=None, polygons=None, mask=None, comment=None, debug=False):
+def create(project_name: str, destination_folder: str = None, polygons=None, mask=None, comment=None, debug=False):
+    """
+    Create a RAPIDA project in a new folder. As default, it creates a project folder with the same name as user specified.
+
+    Usage:
+
+    rapida create [project name] --polygons /path/to/vector_polygons.gpkg --mask /path/to/mask.gpkg
+
+        This creates a project folder at current working directory with project polygon of a given GeoPackage.
+
+        If --mask is provided, it creates a mask layer in the project database, and assessment can compute affected version of variables.
+
+    rapida create [project name] [destination folder] --polygons /path/to/vector_polygons.gpkg
+
+        If you set a folder path in the second positional argument, it creates a project folder at a given destination folder.
+
+        Second positional argument is optional. If skipped, current directory is used.
+
+    """
     setup_logger(name='rapida', level=logging.DEBUG if debug else logging.INFO)
 
     if not is_rapida_initialized():
         return
 
-    abs_folder = os.path.abspath(name)
-    if os.path.exists(abs_folder):
-        logger.error(f'Folder "{name}" already exists in {os.getcwd()}')
+    project_folder = os.path.abspath(project_name)
+    if destination_folder is not None:
+        parent_folder = os.path.abspath(destination_folder)
+        project_folder = os.path.join(parent_folder, project_name)
+
+    if os.path.exists(project_folder):
+        logger.error(f'Folder "{project_name}" already exists in {os.getcwd()}')
         sys.exit(1)
     else:
-        os.mkdir(abs_folder)
+        os.makedirs(project_folder)
 
-    project = Project(path=abs_folder, polygons=polygons, mask=mask, comment=comment)
+    project = Project(path=project_folder, polygons=polygons, mask=mask, comment=comment)
     assert project.is_valid
     logger.info(f'Project "{project.name}" was created successfully.')

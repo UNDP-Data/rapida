fcfbc638fe895592d17534f559605087eb310b1a
Thuhaa <josephthuha@gmail.com>
2025-06-04 11:50:43 +0300

    fix: remove ocha

--- rapida/cli/admin.py
+++ rapida/cli/admin.py
@@ -112,67 +112,6 @@ def osm(bbox=None,admin_level=None, osm_level=None, clip=False, h3id_precision=7
     save(geojson_dict=geojson, dst_path=destination_path, layer_name=f'admin{admin_level}')
 
 
-
-@admin.command(no_args_is_help=True)
-@click.argument('destination_path', type=click.Path())
-@click.option('-b', '--bbox', required=True, type=BboxParamType(),
-              help='Bounding box xmin/west, ymin/south, xmax/east, ymax/north' )
-@click.option('-l','--admin_level',
-                required=True,
-                type=click.IntRange(min=0, max=2, clamp=False),
-                help='UNDP admin level from where to extract the admin features'
-                )
-@click.option('--clip',
-
-    is_flag=True,
-    default=False,
-    help="Whether to clip the data to the bounding box."
-)
-@click.option(
-    '--h3id-precision',
-    type=int,
-    default=7,
-    show_default=True,
-    help="Precision level for H3 indexing (default is 7)."
-)
-@click.option('--debug',
-
-    is_flag=True,
-    default=False,
-    help="Set log level to debug"
-)
-def ocha(bbox=None,admin_level=None,  clip=False, h3id_precision=7, destination_path=None, debug=False ):
-    """
-    Fetch admin boundaries from OCHA COD
-
-    Retrieves administrative boundaries of a specific level/levels that intersect the area covered iby bbox from
-    OCHA COD database hosted on ArcGIS server
-    The admin_level argument can be an integer or a dictionary.
-
-    In case an integer is supplied, the best effort is made to retrieve admin boundaries from that specific admin
-    level data layer for all countries  whose extent intersects the bounding box
-
-    The OCA COD database is hosted on an ArcGIS server instance and is organized on a per country basis. As a result
-    this function also uses a per country approach to fetch features and merge them into one layer in case the supplied
-    bounding box covers several countries.
-
-    To save the result as a file, for instance, the following command can be executed to extract admin 0 data for Rwanda and Burundi as GeoJSON file:
-
-
-    rapida admin ocha -b 33.681335,-0.131836,35.966492,1.158979 -l 2 --clip /data/admocha.fgb --layer-name abc
-    """
-    setup_logger(name='rapida', level=logging.DEBUG if debug else logging.INFO)
-
-    if not is_rapida_initialized():
-        return
-
-    geojson = fetch_ocha_admin(bbox=bbox, admin_level=admin_level, clip=clip, h3id_precision=h3id_precision)
-    if not geojson:
-        logger.error('Could not extract admin boundaries from OCHA for the provided bbox')
-        return
-    save(geojson_dict=geojson, dst_path=destination_path, layer_name=f'admin{admin_level}')
-
-
 @admin.command(no_args_is_help=True)
 @click.argument('destination_path', type=click.Path())
 @click.option('-b', '--bbox', required=True, type=BboxParamType(),
@@ -219,7 +158,3 @@ def cgaz(bbox=None,admin_level=None,  clip=False, destination_path=None, debug=F
         return
 
     fetch_cgaz_admin(bbox=bbox, admin_level=admin_level, clip=clip, destination_path=destination_path)
-    # if not geojson:
-    #     logger.error('Could not extract admin boundaries from CGAZ for the provided bbox')
-    #     return
-    # save(geojson_dict=geojson, dst_path=destination_path, layer_name=f'admin{admin_level}')

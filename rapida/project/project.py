917ad7c0878d1b7b28ebd687c2d28fd28f308bc2
iferencik <92793531+iferencik@users.noreply.github.com>
2025-06-19 15:00:48 +0200

    use os indep. line sep (#441)
    * use textwrap
    
    * replace \n with os.linesep
    
    ---------
    
    Co-authored-by: Ioan Ferencik <ioan.ferencik@undop.org>

--- rapida/project/project.py
+++ rapida/project/project.py
@@ -497,9 +497,7 @@ class Project:
                     })
 
             descriptions = [f'**{info["layer"]}** (Data provider: **{info["attribution"]}**, License: **{info["license"]}**)' for info in layer_info]
-            description = f"""This dataset was generated for the project {self.name} by UNDP RAPIDA tool to assess the following component layers: 
-{"\n".join(f"- {d}" for d in descriptions)}
-"""
+            description = f"This dataset was generated for the project {self.name} by UNDP RAPIDA tool to assess the following component layers:{os.linesep}{os.linesep.join(f'- {d}' for d in descriptions)}"
             attributions = [info["attribution"] for info in layer_info]
             attributions.insert(0, "United Nations Development Programme (UNDP)")
             attribution = ", ".join(attributions)

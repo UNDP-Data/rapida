02071a3bfb0d56d9c6bff54fd515eaa18afe5042
Ioan Ferencik <ioan.ferencik@undop.org>
2025-06-19 14:22:22 +0200

    replace \n with os.linesep

--- rapida/project/project.py
+++ rapida/project/project.py
@@ -8,7 +8,7 @@ import os
 import shutil
 import sys
 import webbrowser
-import textwrap
+
 import pycountry
 import requests
 import click
@@ -497,10 +497,7 @@ class Project:
                     })
 
             descriptions = [f'**{info["layer"]}** (Data provider: **{info["attribution"]}**, License: **{info["license"]}**)' for info in layer_info]
-            description = textwrap.dedent(f"""\
-                This dataset was generated for the project {self.name} by UNDP RAPIDA tool to assess the following component layers:
-                {'\n'.join(f'- {d}' for d in descriptions)}
-            """)
+            description = f"This dataset was generated for the project {self.name} by UNDP RAPIDA tool to assess the following component layers:{os.linesep}{os.linesep.join(f'- {d}' for d in descriptions)}"
             attributions = [info["attribution"] for info in layer_info]
             attributions.insert(0, "United Nations Development Programme (UNDP)")
             attribution = ", ".join(attributions)

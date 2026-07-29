673d40820d7eaf53065b928b0e1aaf617119c9f8
Jin Igarashi <igarashi.jin@water-gis.com>
2026-07-30 06:56:42 +0900

    fix: removed uvloop

--- rapida/cli/__init__.py
+++ rapida/cli/__init__.py
@@ -18,12 +18,6 @@ from rich.progress import Progress
 import click
 import nest_asyncio
 nest_asyncio.apply()
-import uvloop
-import asyncio
-
-asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-asyncio.set_event_loop(asyncio.new_event_loop())
-
 
 
 

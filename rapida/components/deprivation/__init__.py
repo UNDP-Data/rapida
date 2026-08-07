c7dc689e71874e939b8feff556975872f865511a
Ioan Ferencik <ioan.ferencik@undp.org>
2026-08-07 19:04:50 +0200

    adjust gdal_config context to sue all threads due to gdal cache max

--- rapida/components/deprivation/__init__.py
+++ rapida/components/deprivation/__init__.py
@@ -108,7 +108,7 @@ class DeprivationVariable(RwiVariable):
             'GDAL_HTTP_MERGE_CONSECUTIVE_RANGES':'YES',
             'GDAL_CACHEMAX':'512',
             'GDAL_NUMTHREADS':'4'
-        }):
+        }, thread_local=False):
             geo.import_raster(
                 source=local_path, dst=self.local_path, target_srs=project.target_srs,
                 crop_ds=project.geopackage_file_path, crop_layer_name=project.polygons_layer_name,
